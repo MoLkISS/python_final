d0ace0f84f8ade83f7bd82e3dd640041a3cf78dd
MoLkISS <maksimka_ogai@mail.ru>
2023-11-17 22:30:48 +0600

    new files

---
+++ web/app_flask.py
@@ -0,0 +1,13 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+
+db = SQLAlchemy()
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = ''
+
+with app.app_context():
+    db.init_app(app)
+    
+app.config['SECRET_KEY'] = "secret key"
