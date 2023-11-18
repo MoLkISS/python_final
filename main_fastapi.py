8b7bfc624141db32a04f176b60a04d5741faaaca
MoLkISS <maksimka_ogai@mail.ru>
2023-11-18 10:06:48 +0600

    update

--- main_fastapi.py
+++ main_fastapi.py
@@ -4,5 +4,4 @@ from database import SessionLocal, engine
 from fastapi.middleware.cors import CORSMiddleware
 import models, pydantic_validation, crud
 
-models.Base.metadata.create_all(bind_engine)
 models.Base.metadata.create_all(bind=engine)
